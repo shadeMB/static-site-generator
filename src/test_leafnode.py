4ebcbbfd9b1780e55bb177b82f59d0f8b45dfaa4
shadeMB <shade_mbogdan@yahoo.com>
2026-03-22 12:54:06 +0200

    add parent node type and tests

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -2,7 +2,7 @@ import unittest
 
 from leafnode import LeafNode
 
-class TestHTMLNode(unittest.TestCase):
+class TestLeafNode(unittest.TestCase):
     def test_leaf_to_html_p(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
@@ -17,3 +17,6 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_to_html_no_tag(self):
         node = LeafNode(None, "Hello, world!")
         self.assertEqual(node.to_html(), "Hello, world!")
+
+if __name__ == "__main__":
+    unittest.main()
